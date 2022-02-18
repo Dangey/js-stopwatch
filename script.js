e4487625a373133091fd3507e907cfd8601d4621
Anthony <32312177+Dangey@users.noreply.github.com>
2022-02-18 01:11:55 -0500

    Cleans up indenting, Adds main table function recordData() and helper functions

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-window.onload = function () {
-  
+window.onload = function ()
+{
   var seconds = 00; 
   var tens = 00; 
   var appendTens = document.getElementById("tens")
@@ -7,42 +7,46 @@ window.onload = function () {
   var buttonStart = document.getElementById('button-start');
   var buttonStop = document.getElementById('button-stop');
   var buttonReset = document.getElementById('button-reset');
+  let table = document.getElementById('timeTable');
+  let tbodyRef = table.getElementsByTagName('tbody')[0];
   var Interval ;
 
-  buttonStart.onclick = function() {
-    
+  buttonStart.onclick = function()
+  {
     clearInterval(Interval);
-     Interval = setInterval(startTimer, 10);
+    Interval = setInterval(startTimer, 10);
   }
   
-    buttonStop.onclick = function() {
-       clearInterval(Interval);
+  buttonStop.onclick = function()
+  {
+    clearInterval(Interval);
   }
   
-
-  buttonReset.onclick = function() {
-     clearInterval(Interval);
+  buttonReset.onclick = function()
+  {
+    clearInterval(Interval);
     tens = "00";
   	seconds = "00";
     appendTens.innerHTML = tens;
   	appendSeconds.innerHTML = seconds;
   }
   
-   
-  
-  function startTimer () {
+  function startTimer ()
+  {
     tens++; 
     
-    if(tens <= 9){
+    if(tens <= 9)
+    {
       appendTens.innerHTML = "0" + tens;
     }
     
-    if (tens > 9){
+    if (tens > 9)
+    {
       appendTens.innerHTML = tens;
-      
     } 
     
-    if (tens > 99) {
+    if (tens > 99)
+    {
       console.log("seconds");
       seconds++;
       appendSeconds.innerHTML = "0" + seconds;
@@ -50,11 +54,84 @@ window.onload = function () {
       appendTens.innerHTML = "0" + 0;
     }
     
-    if (seconds > 9){
+    if (seconds > 9)
+    {
       appendSeconds.innerHTML = seconds;
     }
-  
   }
   
+  function recordData (op)
+  {
+    let row, cell, rowIndex, cellIndex, color, time;
+    let timeSeconds, timeTens;
+    
+    timeSeconds = seconds;
+    timeTens = tens;
+    
+    rowIndex = tbodyRef.rows.length - 1;
 
+    if (op == "start")
+    {
+      cellIndex = 0;
+      color = "green";
+      row = tbodyRef.insertRow(-1);
+    }
+    else if (op == "stop")
+    {
+      cellIndex = 1;
+      color = "red";
+      row = tbodyRef.rows[rowIndex];
+    }
+    else if (op == "diff")
+    {
+      cellIndex = 2;
+      let startTime, stopTime;
+      color = "blue";
+      row = tbodyRef.rows[rowIndex];
+      
+      startTime = row.cells[0].innerHTML.split(':');
+      stopTime = row.cells[1].innerHTML.split(':');
+      
+      timeSeconds = stopTime[0] - startTime[0];
+      timeTens = stopTime[1] - startTime[1];
+      
+      if (timeTens < 0)
+      {
+        timeTens = 100 + timeTens;
+        timeSeconds--;
+      }
+      
+      //console.log(timeSeconds + " = " + stopTime[0] + " - " + startTime[0]);
+      //console.log(timeTens + " = " + stopTime[1] + " - " + startTime[1]);
+    }
+    
+    if (timeSeconds < 10)
+      timeSeconds = formatTwoPlaces(timeSeconds);
+    if (timeTens < 10)
+      timeTens = formatTwoPlaces(timeTens);
+    
+    time = timeSeconds + ':' + timeTens;
+    
+    cell = row.insertCell(cellIndex);
+    cell.style.color = color;
+    cell.innerHTML = time;
+  }
+  
+  function resetTable ()
+  { 
+    tbodyRef.innerHTML = '';
+  }
+  
+  function disableEnableBtns ()
+  {
+    buttonStart.disabled = !buttonStart.disabled;
+    buttonStop.disabled = !buttonStop.disabled;
+    buttonReset.disabled = !buttonReset.disabled;
+  }
+
+  function formatTwoPlaces (value)
+  {
+     return ("0" + value);
+  }
+  
 }
